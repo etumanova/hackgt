0c531734ba5053414790c6c31da18c694b226692
Anna Fang <annafang300@gmail.com>
2025-09-27 10:33:28 -0400

    added navbar functionality to home and budget pages

---
+++ budget/src/main/java/com/prospero/budget/controller/NavController.java
@@ -0,0 +1,15 @@
+package com.prospero.budget.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class NavController {
+    @GetMapping("/")
+    public String home() {
+        return "index";  // resolves to templates/index.html
+    }
+
+    @GetMapping("/budgeter")
+    public String budgeter() {
+        return "budgeter";  // resolves to templates/budgeter.html
+    }
+}
